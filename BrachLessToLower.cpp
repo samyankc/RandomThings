9f59170068d174485bdd7b0c3f3641b295e533aa
samyankc <80664080+samyankc@users.noreply.github.com>
2022-04-11 19:15:26 +0800

    Update BrachLessToLower.cpp

--- BrachLessToLower.cpp
+++ BrachLessToLower.cpp
@@ -1,5 +1,4 @@
 char BrachLessToLower( char c )
 {
-    // char Transformer = ( c & 0x40 ) >> ( ( 0x7FFFFFE >> ( c & 0x1F ) ) & 1 );
-    return c | ( ( c & 0x40 ) >> ( ( 0x7FFFFFE >> ( c /*& 0x1F*/ ) ) & 1 ) );
+    return c | ( ( c & 0x40 ) >> ( ( 0x7FFFFFE >> ( c & 0x1F ) ) & 1 ) );
 }
