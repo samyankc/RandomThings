b6f8fab603512e061b703cbece222ea946d28026
samyankc <80664080+samyankc@users.noreply.github.com>
2022-04-12 15:03:46 +0800

    Update BranchLessToLower.cpp

--- BranchLessToLower.cpp
+++ BranchLessToLower.cpp
@@ -2,3 +2,10 @@ char BranchLessToLower( char c )
 {
     return c | ( ( c & 0x40 ) >> ( ( 0x7FFFFFE >> ( c & 0x1F ) ) & 1 ) );
 }
+
+
+char ToLower( char c )
+{
+    if ('A'<=c&&c<='Z') c+='a'-'A';
+    return c;
+}
